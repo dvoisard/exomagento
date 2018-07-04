f384717634422c5779161a49c12e26ef4bffa409
David Voisard <voisard.david@gmail.com>
2018-07-04 14:11:07 +0200

    frontend form

--- app/code/local/Swatch/ProductOutOfStockAlert/Block/ProductOutOfStockAlert.php
+++ app/code/local/Swatch/ProductOutOfStockAlert/Block/ProductOutOfStockAlert.php
@@ -8,4 +8,36 @@
  */
 class Swatch_ProductOutOfStockAlert_Block_ProductOutOfStockAlert extends Mage_Core_Block_Template
 {
+    /**
+     * Current product instance
+     *
+     * @var null|Mage_Catalog_Model_Product
+     */
+    protected $_product = null;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTemplate('productoutofstockalert/form.phtml');
+    }
+    
+    /**
+     * Display the form if the product is out of stock
+     * 
+     * @return void
+     */
+    public function displayForm()
+    {
+         if (!$this->_product || $this->_product->isAvailable()) {
+            $this->setTemplate('');
+            return;
+        }
+        
+        $product = Mage::registry('current_product');
+        if ($product && $product->getId()) {
+            $this->_product = $product;
+        }
+
+        return parent::_prepareLayout();
+    }
 }
